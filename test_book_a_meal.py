b614f73af75e284f5d912ad14f087575b7a76909
Ngige Gitau 4fr0c0d3 <ngigemoffat@gmail.com>
2018-04-25 11:10:13 +0300

    Chore/Added tests for update meal and get meal by id [finishes#157078550 #157078511#157059224]

--- test_book_a_meal.py
+++ test_book_a_meal.py
@@ -40,6 +40,47 @@ class TestBookMeal(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('chicken drumsticks', str(response.data))
 
+    def test_get_meal_by_id(self):
+        """Test API endpoint can get a meal using meal_id property (GET/meal/<int: meal_id>) only for admin
+        """
+        mock_meal = self.client.post('/meals/', data=self.meal)
+        self.assertEqual(mock_meal.status_code, 201)
+        result_in_json = json.loads(
+            mock_meal.data.decode('utf-8').replace("'", "\""))
+        result = self.client.get(
+            '/meals/{}'.format(result_in_json['id'])
+        )
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('chicken drumsticks', str(result.data))
+
+    def test_meal_can_be_edited(self):
+        """Test Api endpoint can edit an existing meal (PUT/meals/<int: meal_id>) only for admin
+        """
+        mock_meal = self.client.post(
+            '/meals/',
+            data={'name': 'morning glory',
+                  'category': 'breakfast',
+                  'price': '$8.00',
+                  'image_url': 'www.images.com/2',
+                  'description': 'scotched eggs, french toast, and a sausage'
+
+                  }
+        )
+        self.assertEqual(mock_meal.status_code, 201)
+        mock_meal = self.client.put(
+            '/meals/1',
+            data={'name': 'changamka',
+                  'category': 'breakfast',
+                  'price': '$9.00',
+                  'image_url': 'www.images.com/2',
+                  'description': '2 fried eggs and bacon'
+
+                  }
+        )
+        self.assertEqual(mock_meal.status_code, 200)
+        results = self.client.get('/meals/1')
+        self.assertIn('changamka', str(results.data))
+
 
 if __name__ == '__main__':
     unittest.main()
