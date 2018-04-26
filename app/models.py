f8d67d971357099cd3f35059beb896962d2f31d1
Ngige Gitau 4fr0c0d3 <ngigemoffat@gmail.com>
2018-04-26 12:45:57 +0300

    Feature/Added the model for the meals [finishes #157114806]

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ STATUS = {
 class User(object):
     """The model for the users in the app
     """
-    __CURSOR__ = 1
+    __CURSOR = 1
 
     def __init__(self, username, email, password, status=STATUS['customer']):
         """The constructor for an instance of a user with default status 'customer'
@@ -26,7 +26,7 @@ class User(object):
         self.password_hash = generate_password_hash(password)
         self.status = status
 
-        User.__CURSOR__ += 1
+        User.__CURSOR += 1
 
     def status_caterer(self):
         """Return a user with the status 'caterer'
@@ -64,4 +64,39 @@ class User(object):
         }
         return user_holder
 
-    
+
+class Meal(object):
+    """The model for the meals in the app
+    """
+    __CURSOR = 1
+
+    def __init__(self, name, category, price, image_url, description, caterer: User):
+        """Constructor for the meal with attributes and owner 'caterer'
+        """
+        self.id = Meal.__CURSOR__
+        self.name = name
+        self. category = category
+        self.price = int(price)
+        self.image_url = image_url
+        self.description = description
+        self.caterer = caterer
+        self.date_posted = datetime.utcnow()
+
+        Meal.__CURSOR += 1
+
+    def meal_holder(self):
+        """Return the meal in form of a dict
+        """
+        return {
+            'id': self.id,
+            'owner': self.caterer.id,
+            'meal': {
+                'name': self.name,
+                'category': self.category,
+                'price': self.price,
+                'image_url': self.image_url,
+                'description': self.description,
+                'date_posted': str(self.date_posted)
+            },
+            'order': 0
+        }
