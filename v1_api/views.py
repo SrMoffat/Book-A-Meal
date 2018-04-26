5757ed333768f68569736970424672323c8c5c68
Ngige Gitau 4fr0c0d3 <ngigemoffat@gmail.com>
2018-04-26 19:48:52 +0300

    Feature/GET/api/v1/meals [finishes#157123919]

--- v1_api/views.py
+++ v1_api/views.py
@@ -1,2 +1,40 @@
 """The application views --> no /api/auth views
 """
+from flask import jsonify, make_response, abort, request
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required, current_user
+from app.models import Meal, MockDB, Order
+
+from functools import wraps
+
+
+def clearance_required(access_level):
+    """Decorator for restricting access to resources access_level 1 == 'customer' access_level 2 == 'caterer'
+    """
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not current_user:
+                return make_response(jsonify({"message": "Unauthorized!",
+                                              "status": 401}), 401)
+            if not current_user.allowed_level(access_level):
+                return make_response(jsonify({"message": "Your clearance level is lower than required!",
+                                              "status": 401}), 401)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
+
+class Meal(Resource):
+    """The Meals resource entity for the API
+    """
+
+    @jwt_required
+    @clearance_required(2)
+    def get_meal(self, meal_id):
+        """Gets a single meal querying by id
+        """
+        meal = MockDB.get_meals(meal_id)
+        if not meal:
+            return abort(404)
+        return meal.meal_holder(), 200
