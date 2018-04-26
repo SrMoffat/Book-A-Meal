674b3972efe3fca23d9d273e72966dbda5e7ec37
Ngige Gitau 4fr0c0d3 <ngigemoffat@gmail.com>
2018-04-26 20:00:54 +0300

    Feature/PUT/api/v1/meals [finishes#157126124]

--- v1_api/views.py
+++ v1_api/views.py
@@ -38,3 +38,30 @@ class Meal(Resource):
         if not meal:
             return abort(404)
         return meal.meal_holder(), 200
+
+    @jwt_required
+    @clearance_required(2)
+    def update_meal(self, meal_id):
+        """Updates the meal with the meal_id provided
+        """
+        meal = MockDB.get_meals(meal_id)
+        if not meal:
+            return abort(404)
+        name = request.json.get('name', None)
+        price = request.json.get('price', None)
+        image_url = request.json.get('image_url', None)
+
+        MockDB.meals.remove(meal)
+
+        if name:
+            meal.name = name
+        if price:
+            meal.price = price
+        if image_url:
+            meal.image_url = image_url
+
+        MockDB.meals.append(meal)
+        return {'id': meal_id,
+                'status': '200',
+                'message': 'Meal option succesfully updated!'
+                }, 200
